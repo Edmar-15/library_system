61a4c4f96370bce600248c5a9f3c30d755a8d855
Rym <bugarinrymuel@gmail.com>
2025-12-03 22:37:58 +0800

    pinaltan ko yung template sa login.blade, naggawa ako ng aken, tapos nag lagay ako ng login.css, para sa styling sa may public

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,18 +5,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>LibrarySystem | Login</title>
+    <link rel="stylesheet" href="{{ asset('css/login.css') }}">
 </head>
 <body>
-    <header>
+    {{-- <header>
         <h1>Login</h1>
         <nav>
             <a href="/">Home</a>
         </nav>
-    </header>
+    </header> --}}
     <main>
         <form action="{{ route('login') }}" method="POST" class="form-container">
             @csrf
-            <div class="form-group">
+            {{-- <div class="form-group">
                 <label for="email">Email: </label>
                 <input type="email" name="email" id="email" value="{{ old('email') }}" required>
             </div>
@@ -29,7 +30,23 @@
             </div>
             <div class="form-group">
                 <a href="{{ route('password.request') }}">forgot password?</a>
+            </div> --}}
+
+            <div class="container">
+                <div class="white-back">
+                   
+                </div>
+                <div class="orange-back">
+                    <label for="EMAIL" id="lbl1">Enter Email</label>
+                    <input type="email" id="EMAIL">
+                    <label for="PASS">Enter password</label>
+                    <input type="password" id="PASS">
+                    <a href="#">Forgot password?</a>
+                    <input type="button" value = "confirm">
+                </div>
             </div>
+
+
         </form>
 
         @if ($errors->any())
