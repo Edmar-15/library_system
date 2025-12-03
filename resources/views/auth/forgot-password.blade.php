9eae6aac8889b53d04dbff59ece0bfab97771088
Drei12345678 <0323-3585@lspu.edu.ph>
2025-12-03 22:39:43 +0800

    nagdagdag ako sa code ng forgot may mga comments naman doon, okay na rin ata yung forgot.css sa may public

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -4,20 +4,54 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="{{ asset('css/forgot.css') }}">
 </head>
 <body>
-    <h2>Forgot Password</h2>
+<!--tinanggal ko yung h2 rito-->
 
 @if(session('status'))
     <p>{{ session('status') }}</p>
 @endif
+<!--simula bago-->
+<div class="container"> 
+    <div class="card-wrapper"> 
 
-<form action="{{ route('password.email') }}" method="POST">
-    @csrf
-    <input type="email" name="email" placeholder="Enter your email">
-    @error('email') <p>{{ $message }}</p> @enderror
+        <!-- Left white curved box-->
+        <div class="left-panel"></div>
 
-    <button type="submit">Send Reset Link</button>
-</form>
+        <!-- Orange box -->
+        <div class="forgot-card">
+            <h2>Forgot password?</h2>
+            <p class="subtitle">Don’t worry we got you covered</p>
+
+
+            <form action="{{ route('password.email') }}" method="POST">
+                @csrf
+
+               
+                <div class="input-wrapper">
+                    <input type="email" name="email" placeholder="Enter email : example@gmail.com">
+
+                  
+                    <span class="close-btn">✖</span>
+                </div>
+
+                @error('email')
+                    <p>{{ $message }}</p>
+                @enderror
+
+                
+                <p class="try-way">Try another way?</p>
+
+                
+                <button type="submit" class="confirm-btn">Confirm</button>
+            </form>
+
+        </div> <!-- End orange card -->
+
+    </div> <!-- End card wrapper -->
+</div>
+<!--end-->
+
 </body>
 </html>
