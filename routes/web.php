471dfce843b4ad11b5bd45c4e89b14f04579ad96
Edmar-15 <0323-3644@lspu.edu.ph>
2025-12-03 16:08:46 +0800

    login register forms

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/librarysystem/login', [AuthController::class, 'showLogin'])->name('show.login');
+Route::get('/librarysystem/register', [AuthController::class, 'showRegister'])->name('show.register');
+Route::post('/librarysystem/login')->name('login');
+Route::post('/librarysystem/register')->name('register');
