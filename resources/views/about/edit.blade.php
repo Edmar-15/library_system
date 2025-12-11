7f162f413a4d855197cff49bcde77fcc58c44c72
Rym <bugarinrymuel@gmail.com>
2025-12-11 13:10:46 +0800

    done designing edit page

--- resources/views/about/edit.blade.php
+++ resources/views/about/edit.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>LibrarySystem | Edit About Page</title>
+     <link rel="stylesheet" href="{{ asset('css/edit.css') }}">
 </head>
 <body>
     <header>
